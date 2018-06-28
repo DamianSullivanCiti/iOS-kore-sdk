d10c1a1c4d1a3e9a0255049bf385b46e94413700
Damian Sullivan <damian.sullivan@citi.com>
2018-06-28 22:11:11 +0100

    Revert "Added snippet to launch app home screen after GD authorization is complete. TODO - Check why it is not working without adding this snippet."
    This reverts commit 88a56b6243e2f4770781ac9cc0dafd1d3601758d.

--- KoreBotSDK/KoreBotSDKDemo/KoreBotSDKDemo/KoreGDiOSDelegate.swift
+++ KoreBotSDK/KoreBotSDKDemo/KoreBotSDKDemo/KoreGDiOSDelegate.swift
@@ -103,15 +103,7 @@ class KoreGDiOSDelegate: NSObject, GDiOSDelegate {
                 let appLaunchViewController: AppLaunchViewController = AppLaunchViewController(nibName: "AppLaunchViewController", bundle: nil)
                 let navigationController: UINavigationController = UINavigationController(rootViewController: appLaunchViewController)
                 self.rootViewController = navigationController
-                //TODO: Find out why below LOC not working.
-                //self.appDelegate!.window?.rootViewController = navigationController
-
-                let window = UIWindow(frame: UIScreen.main.bounds)
-                window.backgroundColor = UIColor.white
-                window.rootViewController = navigationController
-
-                self.appDelegate!.window = window
-                self.appDelegate!.window?.makeKeyAndVisible()
+                self.appDelegate!.window?.rootViewController = navigationController
                 
                 hasAuthorized = true
                 didAuthorize()
